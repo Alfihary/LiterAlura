d1f05d24af4779f8be10b77d552ddac4a94addba
Jesús Guevara <95239167+Alfihary@users.noreply.github.com>
2024-07-12 15:52:38 -0600

    actualización de Archivos

---
+++ src/main/java/com/jgma/catalogolibros/CatalogolibrosApplication.java
@@ -0,0 +1,32 @@
+package com.jgma.catalogolibros;
+
+import com.jgma.catalogolibros.principal.Principal;
+import com.jgma.catalogolibros.repository.AutoresRepository;
+import com.jgma.catalogolibros.repository.LibrosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CatalogolibrosApplication implements CommandLineRunner {
+
+	@Autowired
+	private LibrosRepository librosRepository;
+	@Autowired
+	private AutoresRepository autoresRepository;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogolibrosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(librosRepository, autoresRepository);
+
+			principal.miMenu();
+	}
+}
---
+++ src/test/java/com/jgma/catalogolibros/CatalogolibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgma.catalogolibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogolibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
